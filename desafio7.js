1f8f473dce8085e9ac8e9cc3cbf56d62da8af71e
Jesus Gomez <gomjesal@gmail.com>
2024-01-28 18:26:06 -0300

    Crea el desafio7.js

---
+++ desafio7.js
@@ -0,0 +1,7 @@
+let edad = prompt("¡Por favor ingresa tu edad!");
+
+if (edad >= 18) { 
+    console.log ("¡El usuario es mayor de edad!"); 
+} else { 
+    console.log ("¡El usuario es menor de edad!"); 
+}
