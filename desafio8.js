0bb937788f8a795903cd9ddd10dafc03e3f58e74
Jesus Gomez <gomjesal@gmail.com>
2024-01-28 18:28:40 -0300

    Crea el desafio8.js

---
+++ desafio8.js
@@ -0,0 +1,11 @@
+let numero = prompt ("¡Por favor ingresa un número!");
+
+if (numero <0) { 
+    alert ("¡El número introducido es negativo!"); 
+} else { 
+    if (numero == 0) { 
+        alert ("¡El número intoducido es cero!"); 
+    } else { 
+        alert ("¡El número introducido es positivo!");
+    } 
+}
